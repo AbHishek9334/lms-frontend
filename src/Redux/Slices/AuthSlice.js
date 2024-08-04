4d10ff7b6b821be25b4c491639456bc61af85cac
AbHishek9334 <narayana333434@gmail.com>
2024-08-04 07:54:21 +0530

    added sign in functionality

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -27,10 +27,41 @@ export const createAccount=createAsyncThunk("/auth/signup",async (data)=>{
         toast.error(error?.response?.data?.message)
     }
 })
+export const login=createAsyncThunk("/auth/login",async (data)=>{
+    try{
+        const res=axiosInstance.post("user/login",data);
+        toast.promise(res,{
+            loading:"Wait:authenticating your account",
+            success:(data)=>{
+                console.log("The value of data is",data)
+                return data?.data?.message
+            },
+            error:"Failed to login",
+                
+                
+            
+             
+        })
+        return (await res).data
+    }
+    catch(error){
+        toast.error(error?.response?.data?.message)
+    }
+})
 const authSlice=createSlice({
     name:"auth",
     initialState,
-    reducers:{}
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder.addCase(login.fulfilled,(state,action)=>{
+            localStorage.setItem("data",JSON.stringify(action?.payload?.user))
+            localStorage.setItem("isLoggedIn",true)
+            localStorage.setItem("role",action?.payload?.user?.role)
+            state.isLoggedIn=true
+            state.data=action?.payload?.user
+            state.role=action?.payload?.user?.role
+        })
+    }
 
 })
 export default authSlice.reducer
