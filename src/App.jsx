3c75f68b1f32838a203a1c3af95de0c41f0ec5bf
AbHishek9334 <narayana333434@gmail.com>
2024-08-05 10:10:38 +0530

    added require auth

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import CourseList from './Pages/Course/CourseList'
 import Contact from './Pages/Contact'
 import Denied from './Pages/Denied'
 import CourseDescription from './Pages/Course/CourseDescription'
+import RequireAuth from './Components/Auth/RequireAuth'
+import CreateCourse from './Pages/Course/CreateCourse'
 
 
 function App() {
@@ -28,6 +30,9 @@ function App() {
         <Route path="/courses" element={<CourseList/>}></Route>
         <Route path="/courses/description" element={<CourseDescription/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+        <Route element={<RequireAuth allowedRoles={["ADMIN"]}/>}>
+          <Route path="/course/create" element={<CreateCourse/>}></Route>
+        </Route>
         <Route path="/contact" element={<Contact/>}></Route>
         <Route path="/denied" element={<Denied/>}></Route>
         <Route path="*" element={<NotFound/>}></Route>
---
+++ src/Pages/Course/CreateCourse.jsx
@@ -0,0 +1,9 @@
+function CreateCourse(){
+    return(
+        <>
+            hello i am create course page , welcome back 
+        </>
+    )
+
+}
+export default CreateCourse
