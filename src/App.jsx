b41bace8679d26a345657550913367ce8607dbc0
AbHishek9334 <narayana333434@gmail.com>
2024-08-06 18:57:30 +0530

    successfully implemented the edit profile page

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import CourseDescription from './Pages/Course/CourseDescription'
 import RequireAuth from './Components/Auth/RequireAuth'
 import CreateCourse from './Pages/Course/CreateCourse'
 import Profile from './Pages/User/Profile'
+import EditProfile from './Pages/User/EditProfile'
 
 
 function App() {
@@ -36,6 +37,8 @@ function App() {
         </Route>
         <Route element={<RequireAuth allowedRoles={["ADMIN","USER"]}/>}>
             <Route path="/user/profile" element={<Profile/>}></Route>
+            <Route path="/user/editprofile" element={<EditProfile/>}></Route>
+
         </Route>
         <Route path="/contact" element={<Contact/>}></Route>
         <Route path="/denied" element={<Denied/>}></Route>
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -4,7 +4,7 @@ import {toast} from "react-hot-toast";
 const initialState={
     isLoggedIn:localStorage.getItem("isLoggedIn") || false,
     role:localStorage.getItem("role") || "",
-    data:JSON.parse(localStorage.getItem("data")) || {}
+    data:localStorage.getItem("data") != undefined ? JSON.parse(localStorage.getItem("data")) : {}
 }
 export const createAccount=createAsyncThunk("/auth/signup",async (data)=>{
     try{
@@ -63,6 +63,32 @@ export const logout=createAsyncThunk("/auth/logout",async ()=>{
         toast.error(error?.response?.data?.message)
     }
 })
+export const updateProfile=createAsyncThunk("/auth/update/profile",async (data)=>{
+    try{
+        const res=axiosInstance.put("/user/update",data)
+        toast.promise(res,{
+            loading:"Wait:profile update in progress",
+            success:(data)=>{
+                return data?.data?.message
+            },
+            error:"Failed to update the profile"
+        })
+        return (await res).data
+    }
+    catch(error){
+        toast.error(error?.response?.data?.message)
+    }
+})
+export const getUserData=createAsyncThunk("/user/details",async ()=>{
+    try{
+        const res=axiosInstance.get("/user/me")
+        return (await res).data
+        
+    }
+    catch(error){
+        toast.error(error.message)
+    }
+})
 const authSlice=createSlice({
     name:"auth",
     initialState,
@@ -81,6 +107,16 @@ const authSlice=createSlice({
             state.data={}
             state.isLoggedIn=false
             state.role=""
+        })
+        .addCase(getUserData.fulfilled,(state,action)=>{
+            // if(!action?.payload?.user) return
+            localStorage.setItem("data",JSON.stringify(action?.payload?.user))
+            localStorage.setItem("isLoggedIn",true)
+            localStorage.setItem("role",action?.payload?.user?.role)
+            state.isLoggedIn=true
+            state.data=action?.payload?.user
+            state.role=action?.payload?.user?.role
+
         })
     }
 
